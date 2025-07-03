66e4be9a7d3f7175f7b41fc1c986fdaa2956c0d5
felipelima <153960662+felipelima-Ti@users.noreply.github.com>
2025-07-03 19:26:24 -0300

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -197,7 +197,7 @@ const main = async () => {
               "hambúrguerer (100% carne frango),alface,tomate e pão com gergelim.",
             price: 36.2,
             imageUrl:
-              "https://github.com/felipelima-Ti/restaurante/blob/main/public/frango2.png?raw=true",
+              "https://github.com/felipelima-Ti/restaurante/blob/main/public/burgue-frangoo.png?raw=true",
             ingredients: [
               "Pão com gergelim",
               "alface",
